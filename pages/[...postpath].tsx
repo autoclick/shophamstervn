aab687e978884c927c4228836e9c2a22984a07d2
Auto Click <autoclick.us@gmail.com>
2023-05-02 17:08:29 +0700

    test  ignore featureimage

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -25,7 +25,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	}
 	const query = gql`
 		{
-			post(id: "/20-buc-anh-cuc-hai-huoc/", idType: URI) {
+			post(id: "/${path}/", idType: URI) {
 				id
 				excerpt
 				title
