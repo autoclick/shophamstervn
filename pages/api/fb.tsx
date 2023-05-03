1d89a49908127179e40f6d1551f9ba7b98a98213
Auto Click <autoclick.us@gmail.com>
2023-05-03 10:47:57 +0700

    200

--- pages/api/fb.tsx
+++ pages/api/fb.tsx
@@ -3,7 +3,7 @@ import { NextRequest } from 'next/server'
 
 export default async function handler(req: NextRequest) {
   return new ImageResponse(<>{'Not Found"'}</>, {
-    width: 1200,
-    height: 1200,
+    width: 200,
+    height: 200,
   });
 }
