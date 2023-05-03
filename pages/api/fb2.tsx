1db528b7db6e1010615d97c2acba160f691c8e7e
Auto Click <autoclick.us@gmail.com>
2023-05-04 04:59:39 +0700

    update ImageResponse

--- pages/api/fb2.tsx
+++ pages/api/fb2.tsx
@@ -12,77 +12,94 @@ export default async function handler(req: NextRequest) {
       height: 1200,
     })
   }
-  const _return_array=decodeURIComponent(photos).split(",") as Array<string>;
+  const _return_array = decodeURIComponent(photos).split(",") as Array<string>;
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 60,
-          color: 'white',
-          background: '#f6f6f6',
-          width: '100%',
-          height: '100%',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          display: 'flex',
-        }}
-      >
-        <div
-          style={
-            {
-              display: 'flex',
-              flexDirection: 'row',
-            }}
-        >
-          <img style={{ right: 5, bottom: 5, objectFit: 'cover' }}
-            alt="avatar"
-            width="600"
-            height="800"
-            src={_return_array[0]}
-          /> <img style={{ bottom: 5, objectFit: 'cover' }}
-            alt="avatar"
-            width="600"
-            height="800"
-            src={_return_array[1]}
-          />
-
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-          }}
-        >
-          <img style={{ right: 10, objectFit: 'cover' }}
-            alt="avatar"
-            width="400"
-            height="400"
-            src={_return_array[2]}
-          />
-          <img style={{ right: 5, objectFit: 'cover' }}
-            alt="avatar"
-            width="400"
-            height="400"
-            src={_return_array[3]}
-          />
-        </div>
-        <div style={
-          {
-            display: 'flex'
-          }}>
-          <img style={{ objectFit: 'cover' }}
-            alt="avatar"
-            width="400"
-            height="400"
-            src={_return_array[4]}
-          />
-          <div style={{ display: 'flex', position: 'absolute', alignItems: 'center', justifyContent: 'center', top: '0', left: '0', right: '0', bottom: '0', }}>
-            <span style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', background: 'black', opacity: '0.4', }}> </span>
-            <span> +6 </span>
-          </div>
-        </div>
-      </div>
+ <div
+ style={{
+   fontSize: 60,
+   color: 'white',
+   background: '#f6f6f6',
+   width: '100%',
+   height: '100%',
+   flexDirection: 'column',
+   justifyContent: 'center',
+   alignItems: 'center',
+   display: 'flex',
+ }}
+ >
+<div
+ style={{
+   display: 'flex',
+   flexDirection: 'row',
+ }}
+ >
+<img style={{ right: 5,bottom: 5,objectFit:'cover' }}
+   alt="avatar"
+   width="600"
+   height="800"
+   src={_return_array[0]}
+ /><img style={{ bottom: 5,objectFit:'cover' }}
+   alt="avatar"
+   width="600"
+   height="800"
+   src={_return_array[1]}
+ />
+ 
+</div>
+<div
+ style={{
+   display: 'flex',
+   flexDirection: 'row',
+ }}
+ >
+ <img style={{ right: 10,objectFit:'cover' }}
+     alt="avatar"
+   width="400"
+   height="400"
+     src={_return_array[2]}
+   />
+   <img style={{ right: 5,objectFit:'cover' }}
+   alt="avatar"
+   width="400"
+   height="400"
+   src={_return_array[3]}
+ />
+  <div
+ style={{
+   display: 'flex'
+ }}>
+ <img
+   alt="avatar"
+   width="400"
+   height="400"
+   src={_return_array[4]}
+ />
+ <div
+ style={{
+   display: 'flex',
+   position: 'absolute',
+   alignItems: 'center',
+   justifyContent: 'center',
+   top: '0',
+   left: '0',
+   right: '0',
+   bottom: '0',
+ }}>
+ <span style={{
+   position: 'absolute',
+   top: '0',
+   left: '0',
+   width: '100%',
+   height: '100%',
+   background: 'black',
+   opacity: '0.4',
+ }}></span>
+ <span>+6</span>
+ </div>
+ </div>
+</div>
+</div>
     ),
     {
       width: 1200,
