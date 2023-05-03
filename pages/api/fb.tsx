a19bc26cc8edab898cfb47807a2f061576192304
Auto Click <autoclick.us@gmail.com>
2023-05-03 10:54:05 +0700

    latest next js fix error

--- pages/api/fb.tsx
+++ pages/api/fb.tsx
@@ -1,6 +1,8 @@
 import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
-
+export const config = {
+  runtime: 'edge',
+}
 export default async function handler(req: NextRequest) {
   return new ImageResponse(<>{'Not Found"'}</>, {
     width: 200,
