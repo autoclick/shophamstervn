875ec89679d2adf8dd37660df0f181021650d04d
Auto Click <autoclick.us@gmail.com>
2023-06-27 18:54:11 +0700

    Update fb2.tsx

--- pages/api/fb2.tsx
+++ pages/api/fb2.tsx
@@ -16,6 +16,9 @@ export default async function handler(req: NextRequest) {
   }
   const _rand=Math.floor(Math.random() * 45) + 45;
   const _return_array = decodeURIComponent(photos).split(",") as Array<string>;
+  for (let i = 0; i < _return_array.length; i++) {
+    _return_array[i]= _return_array[i].replace(/\?.*/,'');;
+  }
   return new ImageResponse(
     (
  <div
