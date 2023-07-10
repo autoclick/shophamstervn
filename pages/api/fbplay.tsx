cc6644940e725e3579822aed7ea21e71b0d46f45
Auto Click <autoclick.us@gmail.com>
2023-07-10 16:39:14 +0700

    url

--- pages/api/fbplay.tsx
+++ pages/api/fbplay.tsx
@@ -15,14 +15,28 @@ export default async function handler(req: NextRequest) {
       height: 1000,
     })
   }
-  const photo=decodeURIComponent(photo1).replace(/\?.*/,'');;
+  const photo="url("+decodeURIComponent(photo1).replace(/\?.*/,'')+")";
   return new ImageResponse(
     (
+      <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundRepeat:'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: '100%',
+        backgroundImage: '${photo}',
+      }}
+    >
       <img 
         width="150"
         height="150"
-        src={photo}
+        src="https://vn.healthywithdanny.com/wp-content/uploads/play.png"
       />
+    </div>
     ),
     {
       width: 1000,
