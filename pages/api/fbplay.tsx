4518699a5c4bf4357187552d821c251aaf710b4b
Auto Click <autoclick.us@gmail.com>
2023-07-10 16:55:45 +0700

    photo

--- pages/api/fbplay.tsx
+++ pages/api/fbplay.tsx
@@ -18,9 +18,25 @@ export default async function handler(req: NextRequest) {
   const photo="url("+decodeURIComponent(photo1).replace(/\?.*/,'')+")";
   return new ImageResponse(
     (
-      <span style={{ fontSize: 10, fontWeight: 50,
+      <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
         justifyContent: 'center',
-        alignItems: 'center' }}>{photo}</span>
+        alignItems: 'center',
+        backgroundRepeat:'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: '100%',
+        backgroundImage: '{photo}',
+      }}
+    >
+      <img 
+        width="150"
+        height="150"
+        src="https://vn.healthywithdanny.com/wp-content/uploads/play.png"
+      />
+    </div>
     ),
     {
       width: 1000,
