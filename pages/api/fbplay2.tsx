7c498260c07127ac616bc8f24099e24131601977
Auto Click <autoclick.us@gmail.com>
2023-07-12 01:19:52 +0700

    contain

--- pages/api/fbplay2.tsx
+++ pages/api/fbplay2.tsx
@@ -27,7 +27,7 @@ export default async function handler(req: NextRequest) {
         alignItems: 'center',
         backgroundRepeat:'repeat',
         backgroundPosition: 'center',
-        backgroundSize: 'cover',
+        backgroundSize: 'contain',
         backgroundColor:'black',
         backgroundImage: `url(${photo})`,
       }}
